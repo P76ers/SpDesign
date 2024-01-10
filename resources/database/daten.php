04aaea2d6b25044ecfd5173660492e36a4ab9e58
P <117830434+P76ers@users.noreply.github.com>
2024-01-10 15:52:57 +0100

    add dbRecords

--- resources/database/daten.php
+++ resources/database/daten.php
@@ -18,5 +18,41 @@ createDB("suits");
 // insertInto("kunden", "'P'", "'pw3'", "'p@gmail.com'", "'admin'");
 // insertInto("kunden", "'dummy'", "'pw4'", "'dummy@gmail.com'", "'none'");
 
+// insertInto("artikel", "'Roma Anzug sand'", "'Pure S110er Schurwolle von Vitale Barberis Canonico, Italien'", 498, "'1.png'", 0, 1);
+
+// insertInto("artikel", "'Havana Perennial Anzug taupe Fischgrätmuster'", "'Schurwolle Seide von Rogna, Italien'", 379, "'2.png'", 0, 1);
+
+// insertInto("artikel", "'Havana Anzug Dreiteiler navy Hahnentrittmuster'", "'Wolle Kaschmir von E.Thomas, Italien'", 719, "'3.png'", 0, 1);
+
+// insertInto("artikel", "'Penny Loafer dunkelbraun'", "'Italienisches Kalbvelours'", 279, "'4.png'", 0, 2);
+
+// insertInto("artikel", "'Double Monk Schuhe schwarz'", "'Italienisches Kalbsleder'", 279, "'5.png'", 0, 2);
+
+// insertInto("artikel", "'Sneaker grau monochrom
+// '", "'Italienisches Kalbvelours'", 169, "'6.png'", 0, 2);
+
+// insertInto("artikel", "'Mantel grün'", "'Pure Schurwolle'", 379, "'7.png'", 0, 3);
+
+// insertInto("artikel", "'Mantel dunkelgrau Gürtel'", "'Lamawolle Schurwolle von Piacenza, Italien'", 699, "'8.png'", 0, 3);
+
+// insertInto("artikel", "'Mantel mittelbraun'", "'Reines Kamelhaar von Corrado, Italien'", 699, "'9.png'", 0, 3);
+
+// insertInto("artikel", "'Firenze Hose off-white'", "'Reine Baumwolle von Di Sondrio, Italien'", 149, "'10.png'", 0, 4);
+
+// insertInto("artikel", "'Vigo Hose sand Bundfalte'", "'Pure 4fach Traveller Schurwolle von Rogna, Italien'", 179, "'11.png'", 0, 4);
+
+// insertInto("artikel", "'Milano Hose taupe'", "'Circular Schurwolle-Flanell von Vitale Barberis Canonico, Italien'", 169, "'12.png'", 0, 4);
+
+// insertInto("artikel", "'Hemd weiß Doppelmanschette Slim'", "'Ägyptische Baumwolle von Albini, Italien'", 99, "'13.png'", 0, 5);
+
+// insertInto("artikel", "'Westernhemd off-white'", "'Moleskin aus ägyptischer Baumwolle von Pontoglio, Italien'", 149, "'14.png'", 0, 5);
+
+// insertInto("artikel", "'Oxford Hemd hellblau gestreift Extra Slim Fit'", "'Traveller aus ägyptischer Baumwolle von Weba, Schweiz'", 99, "'15.png'", 0, 5);
+
+// insertInto("artikel", "'Krawatte hellbraun'", "'Reine Seide'", 49, "'16.png'", 0, 6);
+
+// insertInto("artikel", "'Einstecktuch weiß'", "'Reine Seide'", 29, "'17.png'", 0, 6);
+
+// insertInto("artikel", "'Gürtel schwarz'", "'Italienisches Rindsleder'", 59, "'18.png'", 0, 6);
 
 ?>
