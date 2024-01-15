71867ae4102d0bd0538fb4ca4198d346180fde72
P <117830434+P76ers@users.noreply.github.com>
2024-01-15 09:03:14 +0100

    edit modal logIn

--- produkte.php
+++ produkte.php
@@ -10,16 +10,12 @@ include($_SERVER['DOCUMENT_ROOT'] . "/SpDesign/resources/layout/header.php");
 
 <br>
 <?php
+
+// echo '<pre>', var_dump($_SESSION), '</pre>';
+// echo '<pre>', var_dump($_POST), '</pre>';
+
 include($_SERVER['DOCUMENT_ROOT'] . "/SpDesign/resources/layout/table.php");
-if (isset($_SESSION["show"]) && $_SESSION["show"] == true) {
-  unset($_SESSION["show"]);
-  $header = "Success";
-  $text = "Sie sind eingeloggt";
-  $class = "alert-success";
-  $redirect = false;
-  $timerShow = 2000;
-  include($_SERVER['DOCUMENT_ROOT'] . "/SpDesign/resources/layout/modal.php");
-} else if (isset($_SESSION["newUser"]) && $_SESSION["newUser"] == true) {
+if (isset($_SESSION["newUser"]) && $_SESSION["newUser"] == true) {
   unset($_SESSION["newUser"]);
   $header = "Success";
   $text = "Sie haben sich erfolgreich registriert!";
@@ -27,16 +23,26 @@ if (isset($_SESSION["show"]) && $_SESSION["show"] == true) {
   $redirect = false;
   $timerShow = 2000;
   include($_SERVER['DOCUMENT_ROOT'] . "/SpDesign/resources/layout/modal.php");
-} else if (isset($_SESSION["anfrage"]) && $_SESSION["anfrage"] == true) {
+} else if (isset($_SESSION["anfrage"]) && $_SESSION["anfrage"] == true && isset($_SESSION["datum"])) {
   $header = "Anfrage gesendet";
   $text = "Termin zum " . $_SESSION["datum"] . " wird bearbeitet";
   $class = "alert-success";
   $redirect = false;
-  $timerShow = 5000;
+  $timerShow = 3000;
+  unset($_SESSION["show"]);
+  include($_SERVER['DOCUMENT_ROOT'] . "/SpDesign/resources/layout/modal.php");
+} else if (isset($_SESSION["show"]) && $_SESSION["show"] == true) {
+  unset($_SESSION["show"]);
+  $header = "Success";
+  $text = "Sie sind eingeloggt";
+  $class = "alert-success";
+  $redirect = false;
+  $timerShow = 2000;
   include($_SERVER['DOCUMENT_ROOT'] . "/SpDesign/resources/layout/modal.php");
 }
 ?>
 
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . "/SpDesign/resources/layout/footer.php");
+unset($_SESSION["datum"]);
 ?>
